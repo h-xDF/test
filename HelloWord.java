00ae127c0e20829ae9dc42f149b4cc813f63fea9
Переяслов Ярослав <y_pereyaslov@eureca.ru>
2016-04-11 15:35:48 +0300

    delete fails

--- HelloWord.java
+++
@@ -1,6 +0,0 @@
-class HelloWord {
-	public static void main(String[] args){
-		System.out.println("11");
-		int a = 42;
-	}
-} 
--- TwoClass.java
+++
@@ -1,6 +0,0 @@
-class Twoclass{
-	public static void tre(int a){
-		HelloWord q = new HelloWord();
-		System.out.print(q);
-	}
-}
